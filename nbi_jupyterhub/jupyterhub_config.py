2933f413bdaf0261530714c488bfde03b48529b1
Rasmus Munk <munk1@live.dk>
2017-11-15 18:41:15 +0000

    With remote header authentication

--- nbi_jupyterhub/jupyterhub_config.py
+++ nbi_jupyterhub/jupyterhub_config.py
@@ -8,6 +8,8 @@ pwd = os.path.dirname(__file__)
 c.JupyterHub.spawner_class = 'cassinyspawner.SwarmSpawner'
 c.JupyterHub.hub_ip = '0.0.0.0'
 
+c.JupyterHub.base_url = '/dag'
+
 c.JupyterHub.cleanup_servers = False
 
 # First pulls can be really slow, so let's give it a big timeout
@@ -17,13 +19,23 @@ c.SwarmSpawner.jupyterhub_service_name = 'nbibda_service_jupyterhub'
 
 c.SwarmSpawner.networks = ["nbibda_service_default"]
 
+notebook_dir = os.environ.get('NOTEBOOK_DIR') or '/home/jovyan/work'
+c.SwarmSpawner.notebook_dir = notebook_dir
+
+mounts = [{'type': 'volume',
+           'source': 'jupyterhub-user-{username}',
+           'target': notebook_dir}]
+
 # 'args' is the command to run inside the service
 c.SwarmSpawner.container_spec = {
     'args': ['/usr/local/bin/start-singleuser.sh'],
     # image needs to be previously pulled
     'Image': '127.0.0.1:5000/nbi_jupyter_notebook',
-    'mounts': []
+    'mounts': mounts
 }
+
+## Authenticator -> remote user header
+c.JupyterHub.authenticator_class = 'jhub_remote_user_authenticator.remote_user_auth.RemoteUserAuthenticator'
 
 # The values here are too low for our OpenStack system
 #c.SwarmSpawner.resource_spec = {
