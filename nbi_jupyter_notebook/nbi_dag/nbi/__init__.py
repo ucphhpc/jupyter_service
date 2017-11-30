3cc8d0a2431d45154b648c1ea9468468e00f89f6
rasmusmunk <munk1@live.dk>
2017-11-30 22:17:51 +0100

    on root request check for mount header

--- nbi_jupyter_notebook/nbi_dag/nbi/__init__.py
+++ nbi_jupyter_notebook/nbi_dag/nbi/__init__.py
@@ -1,6 +1,6 @@
 from notebook.utils import url_path_join
 from notebook.base.handlers import IPythonHandler
-
+import os
 
 def _jupyter_server_extension_paths():
     return [{
@@ -10,13 +10,16 @@ def _jupyter_server_extension_paths():
 
 class HelloWorldHandler(IPythonHandler):
     def get(self):
-        self.log(self.settings['headers'])
-        self.finish('Hello, world!')
+        print("Hello")
+        mount = self.request.headers.get('Mount')
+        if mount is not None:
+            print(mount)
+        self.redirect('/tree')
 
 
 def load_jupyter_server_extension(nbapp):
     nbapp.log.info("nbi enabled")
     web_app = nbapp.web_app
     host_pattern = '.*$'
-    route_pattern = url_path_join(web_app.settings['base_url'], '/hello')
+    route_pattern = url_path_join(web_app.settings['base_url'], '/')
     web_app.add_handlers(host_pattern, [(route_pattern, HelloWorldHandler)])
