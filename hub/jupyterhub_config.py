f2dc5fb0fab9d6823b26476ddf924f817bb522c8
rasmusmunk <munk1@live.dk>
2018-03-12 00:23:57 +0100

    New repo structure

--- hub/jupyterhub_config.py
+++ hub/jupyterhub_config.py
@@ -22,7 +22,7 @@ notebook_dir = os.environ.get('NOTEBOOK_DIR') or '/home/jovyan/work/'
 c.SwarmSpawner.notebook_dir = notebook_dir
 
 mounts = [{'type': 'volume',
-           'driver_config': 'rasmunk/sshfs:next',
+           'driver_config': 'rasmunk/sshfs:latest',
            'driver_options': {'sshcmd': '{sshcmd}', 'id_rsa': '{id_rsa}',
                               'allow_other': '', 'big_writes': '',
                               'reconnect': ''},
@@ -34,14 +34,15 @@ mounts = [{'type': 'volume',
 c.SwarmSpawner.container_spec = {
     'args': ['/usr/local/bin/start-singleuser.sh'],
     # image needs to be previously pulled
-    'Image': '127.0.0.1:5000/nbi_mig_mount_notebook',
+    'Image': 'nielsbohr/nbi_base_notebook',
     'mounts': mounts
 }
 
+# TODO -> Dynamic MOUNT-HOST naming
 # Available docker images the user can spawn
 c.SwarmSpawner.dockerimages = [
-    {'image': '127.0.0.1:5000/nbi_mig_mount_notebook',
-     'name': 'Image with default MiG Homedrive mount, supports Py2/3 and R'}
+    {'image': 'nielsbohr/nbi_base_notebook',
+     'name': 'Image with automatic {mount_host} mount, supports Py2/3 and R'}
 ]
 
 # Authenticator -> remote user header
@@ -53,7 +54,7 @@ c.JupyterHub.authenticator_class = 'jhub_remote_user_authenticator' \
 # During conjestion, kill random internal processes to limit
 # available load to 1 core/ 2GB mem 
 c.SwarmSpawner.resource_spec = {
-    'cpu_limit': int(4 * 1e9),
+    'cpu_limit': int(8 * 1e9),
     'mem_limit': int(8192 * 1e6),
     'cpu_reservation': int(1 * 1e9),
     'mem_reservation': int(2048 * 1e6),
