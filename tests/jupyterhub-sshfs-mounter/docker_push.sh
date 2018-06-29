6a42928cd1f6d7f502963f6bc6899524ebbb789c
Rasmus Munk <munk1@live.dk>
2018-06-29 02:49:02 +0200

    Update dockerhub pust image tag

--- tests/jupyterhub-sshfs-mounter/docker_push.sh
+++ tests/jupyterhub-sshfs-mounter/docker_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker push "$DOCKER_USERNAME"/ssh-mount-dummy:latest
+docker push "$DOCKER_USERNAME"/jupyterhub-sshfs-mounter:latest
